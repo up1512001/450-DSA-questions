5d2eff8d676836622bc84c702faefa96155e5e72
utsav <75293077+up1512001@users.noreply.github.com>
2021-08-08 08:00:13 +0530

    Add files via upload

--- dynamic programming/minimum subset sum difference.cpp
+++ dynamic programming/minimum subset sum difference.cpp
@@ -1,30 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define v vector
 
-void print(v<v<int>>&dp){
-    cout<<"Printing DP Table:\n";
-    for(int i=0;i<dp.size();i++){
-        for(int j=0;j<dp[0].size();j++){
-            cout<<dp[i][j]<<" ";
+int minSubsetDifference(vector<int>&arr,int sum){
+
+    int dp[arr.size()+1][sum+1];
+    for(int i=0;i<arr.size()+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(i==0)
+                dp[i][j] = 0;
+            if(j==0)
+                dp[i][j] = 1;
         }
-        cout<<"\n";
     }
-    cout<<"End of Table:\n";
+
+    for(int i=1;i<arr.size()+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(arr[i-1]<=j){
+                dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+            }else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    vector<int> possibleSum;
+    for(int i=0;i<(sum+1)/2;i++){
+        if(dp[arr.size()][i]){
+            possibleSum.push_back(i);
+        }
+    }
+    int mn=INT_MAX;
+    for(int i=0;i<possibleSum.size();i++){
+        mn = min(mn,sum-2*possibleSum[i]);
+    }
+
+    return mn;
+
 }
 
-
-
-
 int main(){
-    cout<<"Enter size of Array:\n";
+
+    cout<<"Enter Size Of Array:->";
     int n;cin>>n;
-    v<int> arr(n,0);
-    cout<<"Enter Array :\n";
+
+    cout<<"Enter Array:\n";
+    vector<int> arr(n,0);
+    int sum=0;
     for(int i=0;i<n;i++){
         cin>>arr[i];
+        sum+=arr[i];
     }
-    cout<<"Enter finding sum:\n";
-    int t;cin>>t;
+
+    cout<<"Minimum Subset Sum Difference is :->"<<minSubsetDifference(arr,sum)<<"\n";
 
 }
