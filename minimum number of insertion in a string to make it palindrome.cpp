c7b3791e65b37fddaa47b70d3a6686caae4bce35
utsav <75293077+up1512001@users.noreply.github.com>
2021-08-19 22:37:09 +0530

    Add files via upload

---
+++ minimum number of insertion in a string to make it palindrome.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minInsertion(string s){
+
+    string t=s;
+    int dp[s.size()+1][t.size()+1];
+
+    reverse(begin(t),end(t));
+
+    for(int i=0;i<=s.size();i++)
+        for(int j=0;j<=t.size();j++)
+            if(i==0 || j==0)
+                dp[i][j] = 0;
+
+    for(int i=1;i<=s.size();i++){
+        for(int j=1;j<=t.size();j++){
+            if(s[i-1]==t[j-1])
+                dp[i][j] = 1+dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+
+    return s.size() - dp[s.size()][t.size()];
+
+}
+
+int main(){
+
+    cout<<"Enter String :->";
+    string s;cin>>s;
+
+    cout<<"minimum number of insertions to make given string palindrome are :->"<<minInsertion(s)<<"\n";
+
+}
