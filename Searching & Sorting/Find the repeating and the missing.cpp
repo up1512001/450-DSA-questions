e9ed599bf8bf1c89c477a8108857de227cf83541
utsav <75293077+up1512001@users.noreply.github.com>
2021-09-06 22:24:17 +0530

    Add files via upload

--- Searching & Sorting/Find the repeating and the missing.cpp
+++ Searching & Sorting/Find the repeating and the missing.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-pair<int,int> calculateNLogN(vector<int>&arr){
+pair<int,int> calculateNLogN(vector<int>arr){
     pair<int,int> ans;
     sort(arr.begin(),arr.end());
     for(int i=0;i<arr.size()-1;i++){
@@ -15,6 +15,35 @@ pair<int,int> calculateNLogN(vector<int>&arr){
     return ans;
 }
 
+pair<int,int> calculateNTime(vector<int>arr){
+    int xor1 = arr[0];
+    for(int i=1;i<arr.size();i++){
+        xor1 = xor1 ^ arr[i];
+    }
+    for(int i=1;i<=arr.size();i++){
+        xor1 = xor1 ^ i;
+    }
+    int set_bit_no = xor1 & ~(xor1-1);
+    int x=0,y=0;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i] & set_bit_no){
+            x = x ^ arr[i];
+        }else{
+            y = y ^ arr[i];
+        }
+    }
+    for(int i=1;i<=arr.size();i++){
+        if(i & set_bit_no){
+            x = x ^ i;
+        }else{
+            y = y ^ i;
+        }
+    }
+    pair<int,int> p(y,x);
+    return p;
+}
+
+
 int main(){
 
     cout<<"Enter Size of Array:->";
@@ -25,7 +54,9 @@ int main(){
         cin>>arr[i];
     
     pair<int,int> ans = calculateNLogN(arr);
-
+    cout<<"repeating element is "<<ans.first<<" missing is "<<ans.second<<"\n";
+ 
+    ans = calculateNTime(arr);
     cout<<"repeating element is "<<ans.first<<" missing is "<<ans.second<<"\n";
 
 }
